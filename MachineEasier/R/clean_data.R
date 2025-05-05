21aa28cf902e868332ebace57a18a24d367ef4db
Jesse <j.mdnd11@gmail.com>
2025-05-05 16:41:50 -0400

    Update clean_data.R

--- MachineEasier/R/clean_data.R
+++ MachineEasier/R/clean_data.R
@@ -1,14 +1,24 @@
-#' Clean data by removing NA Values
+#' Clean Data by Removing NA Values
 #'
-#' @param data A data frame
-#' @param remove_na Logical. If TRUE, remove rows with NAs.
-#' @return A cleaned data frame
-#' @importFrom tidyr drop_na
+#' This function cleans a data frame by removing rows with missing values (NA) if the `remove_na` parameter is set to TRUE.
+#'
+#' @param data A data frame.
+#' @param remove_na Logical. If TRUE, rows containing NA values are removed. Default is TRUE.
+#'
+#' @return A cleaned data frame with or without NA values, depending on the `remove_na` argument.
 #' @export
+#'
+#' @examples
+#' cleaned_data <- clean_data(mtcars)
 
 clean_data <- function(data, remove_na = TRUE) {
+  if (!is.data.frame(data)) {
+    stop("The input data must be a data frame.")
+  }
+
   if (remove_na) {
     data <- drop_na(data)
   }
+
   return(data)
 }
