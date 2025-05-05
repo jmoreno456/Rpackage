2e783af1d3415363a6bd2caafee6d15965b0586b
Jesse <j.mdnd11@gmail.com>
2025-05-05 00:18:02 -0400

    Update CheckFunction.R

--- MachineEasier/R/CheckFunction.R
+++ MachineEasier/R/CheckFunction.R
@@ -6,7 +6,7 @@ check_functions <- function() {
   required_functions <- c("split_data", "train_model", "evaluate_model", "plot_predictions")
 
   # Check if each function is available
-  missing_functions <- setdiff(required_functions, ls("package:MachineEasier"))
+  missing_functions <- setdiff(required_functions, ls(getNamespace("MachineEasier)))
 
   if (length(missing_functions) == 0) {
     message("All functions are loaded correctly.")
