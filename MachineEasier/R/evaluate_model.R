5a386dfdc3035eeefead590675d9dcd30aa7dba8
Jesse <j.mdnd11@gmail.com>
2025-05-05 16:42:20 -0400

    Update evaluate_model.R

--- MachineEasier/R/evaluate_model.R
+++ MachineEasier/R/evaluate_model.R
@@ -1,18 +1,25 @@
-#' Evaluate a Predictive Model
+#' Evaluate Model Performance
 #'
-#' @param model A trained model object.
-#' @param data A data frame containing the test data.
-#' @param target The target variable (string).
-#' @return A list with RMSE and R² values.
-#' importFrom caret RMSE R2
+#' This function evaluates a fitted model's performance by calculating RMSE (Root Mean Squared Error) and R-squared using the test data.
+#'
+#' @param model A fitted model object created using the `fit()` function from the `parsnip` package.
+#' @param test_data A data frame containing the test data.
+#' @param target The name of the target column (as a string).
+#'
+#' @return A list containing RMSE and R-squared metrics.
 #' @export
+#'
+#' @examples
+#' model <- train_model(mtcars, "mpg", "lm")
+#' metrics <- evaluate_model(model, test_data, "mpg")
 
-evaluate_model <- function(model, data, target) {
-  predicted <- predict(model, newdata = data)
-  actual <- data[[target]]
+evaluate_model <- function(model, test_data, target) {
+  predictions <- predict(model, new_data = test_data) %>% bind_cols(test_data)
 
-  rmse <- RMSE(predicted, actual)
-  r_squared <- R2(predicted, actual)
+  metrics <- list(
+    RMSE = rmse(predictions, truth = !!sym(target), estimate = .pred),
+    R2   = rsq(predictions, truth = !!sym(target), estimate = .pred)
+  )
 
-  return(list(RMSE = rmse, R2 = r_squared))
+  return(metrics)
 }
