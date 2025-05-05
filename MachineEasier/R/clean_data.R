83508ee67be368107b3659b98b5fa8e99fd7146b
Jesse <j.mdnd11@gmail.com>
2025-05-05 00:17:04 -0400

    Update clean_data.R

--- MachineEasier/R/clean_data.R
+++ MachineEasier/R/clean_data.R
@@ -3,11 +3,12 @@
 #' @param data A data frame
 #' @param remove_na Logical. If TRUE, remove rows with NAs.
 #' @return A cleaned data frame
+#' @importFrom tidyr drop_na
 #' @export
 
 clean_data <- function(data, remove_na = TRUE) {
   if (remove_na) {
-    data <- tidyr::drop_na(data)
+    data <- drop_na(data)
   }
   return(data)
 }
